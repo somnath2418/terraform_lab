4476b0e6f237e7ee2d2210fe54391f02ce49d106
somnath <somnath23@gmail.com>
2026-04-23 20:53:29 +0530

    all file edit 5

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "tfstateprod123"
+    storage_account_name = "somseema23tf"
     container_name       = "tfstate"
     key                  = "vm.terraform.tfstate"
   }
